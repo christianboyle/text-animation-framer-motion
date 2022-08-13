b555f271eded99433bda3e2c476993bccc190c55
Christian Boyle <mail@christianboyle.com>
2022-08-13 07:38:11 -0500

    🔨

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # text-animation-framer-motion
+
+Code from Chetan Verma article. [[source](https://blog.chetanverma.com/how-to-create-awesome-text-animation-with-framer-motion)]
--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import AnimatedTextWord from './components/AnimatedTextWord.jsx'
 
 export default function Home() {
   return (
-    <div className='container h-screen mx-auto flex flex-col items-center justify-center'>
+    <div>
       <AnimatedTextWord text='animated text (by word) with framer-motion' />
       <AnimatedTextCharacter text='animated text (by character) with framer-motion' />
     </div>
